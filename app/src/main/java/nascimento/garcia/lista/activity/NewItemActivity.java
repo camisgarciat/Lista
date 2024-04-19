5c13509920e2fe9aee4f920b46d6a55c0f335896
20221tiimi0360 <camisgarciat@icloud.com>
2024-04-19 09:09:18 -0300

    Atividade lista

--- app/src/main/java/nascimento/garcia/lista/activity/NewItemActivity.java
+++ app/src/main/java/nascimento/garcia/lista/activity/NewItemActivity.java
@@ -1,6 +1,9 @@
-package nascimento.garcia.lista;
+package nascimento.garcia.lista.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import nascimento.garcia.lista.R;
+
 public class NewItemActivity extends AppCompatActivity {
+    static int PHOTO_PICKER_REQUEST = 1;
+    Uri photoSelected= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,15 @@ public class NewItemActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        ImageButton imgCl = findViewById(R.id.imbCl);
+        imgCl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST);
+            }
         });
     }
 }
